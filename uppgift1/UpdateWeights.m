0ba0f014690deb79512e5681b30b401a9a9fb135
walline <erik.i.wallin@gmail.com>
2017-10-09 17:33:25 +0200

    neighbourhood function

--- uppgift1/UpdateWeights.m
+++ uppgift1/UpdateWeights.m
@@ -1,5 +1,10 @@
 function networkMatrix = UpdateWeights(networkMatrix,T_order,sigma_0,eta_0)
 
+    function omega = NeighbourhoodFunction(index1,index2,sigma)
+        omega = exp(-abs(index1-index2)^2/(2*sigma^2));
+    end
+        
+
 eta = eta_0*
 
 delta_weights = eta
